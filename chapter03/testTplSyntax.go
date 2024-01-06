98bf86dd489676b3171f3f782b36a95bb4f4724c
gexin950228 <861439031@qq.com>
2024-01-06 15:36:53 +0800

    modified:   chapter03/testTplSyntax.go 	new file:   chapter03/tplFunction.go 	new file:   template/chapter03/base.html 	modified:   template/chapter03/tpl1.html

--- chapter03/testTplSyntax.go
+++ chapter03/testTplSyntax.go
@@ -5,10 +5,22 @@ import (
 	"net/http"
 )
 
+type Article struct {
+	Id    int
+	Title string
+	Desc  string
+}
+
 func Tpl1(ctx *gin.Context) {
+	article := Article{
+		Id:    1,
+		Title: "西游记",
+		Desc:  "西天取经",
+	}
 	mapData := map[string]interface{}{
-		"name": "葛新",
-		"arr":  []int{1, 2, 3, 4, 5},
+		"name":    "葛新",
+		"arr":     []int{1, 2, 3, 4, 5},
+		"article": article,
 	}
 	ctx.HTML(http.StatusOK, "chapter03/tpl1.html", mapData)
 }
